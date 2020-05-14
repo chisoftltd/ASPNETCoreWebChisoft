c8b0de868d615506f96a9737c9266882b6f351c3
chisoftltd <chisoftltd@gmail.com>
2020-05-14 22:58:51 +0200

    Add project files.

---
+++ ASPNETCoreWebChisoft/AddDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ASPNETCoreWebChisoft
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
